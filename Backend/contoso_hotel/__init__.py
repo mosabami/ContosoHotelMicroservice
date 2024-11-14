766ad44bff46d445196d61d835e0f36362bd666e
mosabami <aayodeji@microsoft.com>
2024-11-13 22:01:33 -0800

    initial commit

---
+++ Backend/contoso_hotel/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask  # Import the Flask class
+from flask_cors import CORS  # Import the CORS class
+
+app = Flask(__name__)  # Create an instance of the class for our use
+CORS(app, origins="*")  # Enable CORS for all origins
+app.config['MAX_CONTENT_LENGTH'] = 64 * 1000 * 1000
+
+
+
+
